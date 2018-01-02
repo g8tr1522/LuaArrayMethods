acaa14a89c73d56e2eda2bb4c626a073d99a757d
William Prophet <g8tr1522@gmail.com>
2018-01-02 02:04:36 -0500

    added basic __ipairs metamethod
    So now, pass a lam object to ipairs, and ipairs will work on a copy of the 'table' in the lam object, just like a vanilla lua array

--- src/object_methods/_class_and_meta.lua
+++ src/object_methods/_class_and_meta.lua
@@ -26,6 +26,7 @@ class_and_meta.copy       = require(_root..'copy')
 
 class_and_meta.__len			= require(_root..'__len')
 class_and_meta.__concat   = require(_root..'__concat')
+class_and_meta.__ipairs   = require(_root..'__ipairs')
 
 
 
--- src/object_methods/class_and_meta/__ipairs.lua
+++ src/object_methods/class_and_meta/__ipairs.lua
@@ -2,6 +2,11 @@ return function (lam_obj)
 	local t = lam_obj:gettable()
 	local i = 0
 	
-	local iter = function (t)
+	local iter = function ()
 		i = i+1
 		local v = t[i]
+		if v then return i,v end
+	end
+	
+	return iter, i
+end
