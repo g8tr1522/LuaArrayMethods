bcc2c9ef14aba8ec6093b89cbafebd62c0a924ac
William Prophet <g8tr1522@gmail.com>
2018-01-06 16:53:34 -0500

    got started on 'sample' selector

---
+++ src/basic/selectors/sample.lua
@@ -0,0 +1,13 @@
+-- randomly return elements from an array
+--
+-- `amount` argument:
+-- "nil" : return a single element
+-- "number" : return that many elements
+-- 
+-- also see "sample_" to get unique elements
+--
+
+
+return function (t, amount)
+	
+	
